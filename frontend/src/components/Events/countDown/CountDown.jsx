843da5d93ecba34f6f279de43be7cbb8ce844fd8
SylvinIsamaza <116205933+SylvinIsamaza@users.noreply.github.com>
2023-11-20 15:59:07 +0200

    Solving error found in Coundown

--- frontend/src/components/Events/countDown/CountDown.jsx
+++ frontend/src/components/Events/countDown/CountDown.jsx
@@ -1,44 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-function CountDown({start,end}) {
-    const [timeLeft,setTimeLeft]=useState(calculateTimeLeft());
-  
-    useEffect(()=>{
-       const timer=setTimeout(()=>{
-        setTimeLeft(calculateTimeLeft())
-        },1000)
-        return ()=> clearTimeout(timer)
-    })
-    function calculateTimeLeft(){
-        
-        const difference= new Date(end.slice(0,10))-new Date();
-    
-        const timeLeft={
-            days:Math.floor((difference/(1000*60*60*24))),
-            hours:Math.floor(((difference/(1000*60*60))%24)),
-            minutes:Math.floor(((difference/(1000*60))%60)),
-            seconds:Math.floor((difference/(1000)%60))
-        }
-        if(difference>0){
-        return timeLeft
-        }
-   
+function CountDown({ start, end }) {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
-   
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [timeLeft]);
+
+  function calculateTimeLeft() {
+    const difference = new Date(end.slice(0, 10)) - new Date();
+
+    if (difference <= 0) {
+      // If the difference is not greater than 0, return all zeros
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
     }
-const timeComponent=Object.keys(timeLeft).map((interval,index)=>{
-if(!timeLeft[interval]){
-    return 0
-}
-return <span className=' text-[25px] text-[#475ad2]' key={index}>
-    {timeLeft[interval]}{interval} {" "}
-</span>
-})
+
+    const timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / (1000 * 60)) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+
+    return timeLeft;
+  }
+
+  const timeComponent = Object.keys(timeLeft).map((interval, index) => (
+    <span className='text-[25px] text-[#475ad2]' key={index}>
+      {timeLeft[interval] < 10 ? `0${timeLeft[interval]}` : timeLeft[interval]}{interval}{" "}
+    </span>
+  ));
+
   return (
     <div className='w-full'>
-     {timeComponent?(timeComponent):<span className='text-[red] text-[25px]'>Time is up</span>}
+      {timeComponent.every((value) => value.props.children.startsWith('00')) ? (
+        <span className='text-[red] text-[25px]'>Time is up</span>
+      ) : (
+        timeComponent
+      )}
     </div>
-  )
+  );
 }
 
-export default CountDown
+export default CountDown;
